6f806cfd38671696814e245baafb087e00d4d560
Filipe Alves <filipe.alvesdsouza@gmail.com>
2023-12-30 19:23:22 -0300

    Add quiz page logic

--- src/pages/Question/index.tsx
+++ src/pages/Question/index.tsx
@@ -1,9 +1,11 @@
+import { useNavigate } from 'react-router-dom';
 import { useParams } from 'react-router-dom';
 import { Navigate } from 'react-router-dom';
 import styles from './styles.module.css';
 import { getById } from '../../utils/subjects';
 import { list } from '../../utils/questions';
 import Card from '../../components/Card';
+import { useState } from 'react';
 
 function Question() {
     const { subject: subjectId } = useParams();
@@ -24,6 +26,34 @@ function Question() {
         );
     }
 
+    const [current, setCurrent] = useState(0);
+    const [selected, setSelected] = useState('');
+    const [score, setScore] = useState(0);
+    const navigate = useNavigate();
+
+    function getNextQuestion() {
+        const finished = (current + 1) === questions!.length;
+
+        if (finished === true) {
+            navigate('/resultado');
+            return;
+        }
+
+        setSelected('');
+        setCurrent(prevState => prevState + 1);
+    }
+
+    function handleSend() {
+        const isCorrect = questions![current].answer === selected;
+
+        if (isCorrect === true) {
+            sessionStorage.setItem('SCORE', String(score + 1));
+            setScore(prevState => prevState + 1);
+        }
+
+        getNextQuestion();
+    }
+
     return (
         <div className={`page ${styles.container}`}>
             <header className={styles.header}>
@@ -41,21 +71,48 @@ function Question() {
             <main className={styles.content}>
                 <section className={styles.question}>
                     <p className={styles.counter}>
-                        Pergunta 1 de 10
+                        Pergunta {current + 1} de {questions.length}
                     </p>
 
                     <p className={styles.text}>
-                        Texto da pergunta
+                        {questions[current].text}
                     </p>
                 </section>
 
                 <section className={styles.options}>
-                    <Card text='Opção 1' iconFallback='A' />
-                    <Card text='Opção 2' iconFallback='B' />
-                    <Card text='Opção 3' iconFallback='C' />
-                    <Card text='Opção 4' iconFallback='D' />
+                    <Card
+                        text={questions[current].options.a}
+                        iconFallback='A'
+                        hidden={selected !== '' && selected !== 'a'}
+                        onClick={() => setSelected('a')}
+                    />
 
-                    <button className={styles.button}>
+                    <Card
+                        text={questions[current].options.b}
+                        iconFallback='B'
+                        hidden={selected !== '' && selected !== 'b'}
+                        onClick={() => setSelected('b')}
+                    />
+
+                    <Card
+                        text={questions[current].options.c}
+                        iconFallback='C'
+                        hidden={selected !== '' && selected !== 'c'}
+                        onClick={() => setSelected('c')}
+                    />
+
+                    <Card
+                        text={questions[current].options.d}
+                        iconFallback='D'
+                        hidden={selected !== '' && selected !== 'd'}
+                        onClick={() => setSelected('d')}
+                    />
+
+                    <button
+                        className={styles.button}
+                        disabled={selected === ''}
+                        onClick={handleSend}
+                    >
                         Enviar resposta
                     </button>
                 </section>
