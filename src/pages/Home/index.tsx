78e2f5575916cc666fdd7fd7ebede3a5953a315b
Filipe Alves <filipe.alvesdsouza@gmail.com>
2024-01-11 18:14:44 -0300

    Add home page subjects from backend

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,11 +1,39 @@
 import Welcome from '../../components/HomePage/Welcome';
 import styles from './styles.module.css';
-import {SUBJECTS} from '../../constants/subjects';
 import Card from '../../components/Card';
 import { useNavigate } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import spinnerIcon from '../../assets/icons/spinner.svg';
+import { getAll } from '../../models/subject';
+
+import { ISubject } from '../../constants/subjects.d';
 
 function Home() {
+    const [isLoading, setIsLoading] = useState(false);
+    const [hasLoadError, setHasLoadError] = useState(false);
+    const [subjects, setSubjects] = useState<ISubject[]>([]);
     const navigate = useNavigate();
+
+    async function fetchSubjects() {
+        setHasLoadError(false);
+        setIsLoading(true);
+
+        const data = await getAll();
+
+        if (data !== null) {
+            setSubjects(data);
+        }
+
+        else {
+            setHasLoadError(true);
+        }
+
+        setIsLoading(false);
+    }
+
+    useEffect(() => {
+        fetchSubjects();
+    }, []);
 
     return (
         <div className={`page ${styles.container}`}>
@@ -13,14 +41,32 @@ function Home() {
                 <Welcome />
 
                 <section className={styles.subjects}>
-                    {SUBJECTS.map(subject => (
-                        <Card
-                            text={subject.name}
-                            iconSrc={subject.iconSrc}
-                            key={subject.id}
-                            onClick={() => navigate(`/${subject.id}`)}
-                        />
-                    ))}
+                    {(hasLoadError === false && isLoading === true) && (
+                        <div className={styles.feedback}>
+                            <img src={spinnerIcon} />
+                            <p>Carregando...</p>
+                        </div>
+                    )}
+
+                    {(hasLoadError === true && isLoading === false) && (
+                        <div className={styles.feedback}>
+                            <p>Ocorreu um erro ao carregar os assuntos</p>
+                            <button onClick={fetchSubjects}>
+                                Tentar novamente
+                            </button>
+                        </div>
+                    )}
+
+                    {((hasLoadError === false && isLoading === false)) && (
+                        subjects.map(subject => (
+                            <Card
+                                text={subject.name}
+                                iconSrc={subject.iconSrc}
+                                key={subject.id}
+                                onClick={() => navigate(`/${subject.id}`)}
+                            />
+                        ))
+                    )}
                 </section>
             </main>
         </div>
