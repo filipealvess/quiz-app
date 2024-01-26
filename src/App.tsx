9338333ac420f7436ea1f62a25af5acf4aebb07f
Filipe Alves <filipe.alvesdsouza@gmail.com>
2024-01-26 17:47:13 -0300

    Use store provider in app

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import { BrowserRouter } from 'react-router-dom';
 import Router from './Router';
+import StoreProvider from './contexts/Store';
 
 function App() {
     return (
-        <BrowserRouter>
-            <Router />
-        </BrowserRouter>
+        <StoreProvider>
+            <BrowserRouter>
+                <Router />
+            </BrowserRouter>
+        </StoreProvider>
     );
 }
 
